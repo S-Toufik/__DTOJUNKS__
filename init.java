2cd332bad3c552d40b8530848f3fd5c4c268a0db
SandBox <174260350+S-Toufik@users.noreply.github.com>
2026-02-25 10:50:08 +0100

    Update init.java

--- init.java
+++ init.java
@@ -1,22 +1,58 @@
-@Component
-public class AppWarmup {
-    private final MyService service;
-    private final WebClient webClient;
+package com.example.config;
 
-    public AppWarmup(MyService service, WebClient webClient) {
-        this.service = service;
-        this.webClient = webClient;
+import java.time.Duration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.http.server.HttpServer;
+
+@Configuration
+@RequiredArgsConstructor
+public class NettyWarmupConfiguration {
+
+    private final WebClient webClient; // Inject your real WebClient bean
+
+    @Bean
+    public WebServerFactoryCustomizer<NettyReactiveWebServerFactory> nettyServerWarmup() {
+        return factory -> factory.addServerCustomizers(HttpServer::warmup);
     }
 
+    
+    @Bean
+    public HttpClient httpClient() {
+        return HttpClient.create()
+                         .warmup();
+    }
+
+    
+    @Bean
+    public WebClient webClient(HttpClient httpClient) {
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
+    }
+
+    
     @EventListener(ApplicationReadyEvent.class)
     public void warmup() {
-        // warmup reactive pipeline
-        service.getData().subscribe();
 
-        // warmup webclient
-        webClient.get().uri("/actuator/health")
-                 .retrieve()
-                 .bodyToMono(String.class)
-                 .subscribe();
+        Mono.when(
+                webClient.get()
+                         .uri("http://localhost:8080/actuator/health")
+                         .retrieve()
+                         .bodyToMono(String.class)
+        )
+        .timeout(Duration.ofSeconds(5))
+        .block();  //??
     }
 }
